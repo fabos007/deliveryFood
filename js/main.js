eadb2011c2f184a882d769f4d967eb7b8bac369c
Sviatoslav Prudkyi <sviatoslav.prudkyi@eu.nextiva.local>
2020-05-05 07:58:38 -0700

    created Authorization window, if login is missing, it will show-up red color over the login border

--- js/main.js
+++ js/main.js
@@ -8,3 +8,84 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+//day 1
+let login = localStorage.getItem('gloDelivery')
+const buttonAuth = document.querySelector('.button-auth'),
+  modalAuth = document.querySelector('.modal-auth'),
+  closeAuth = document.querySelector('.close-auth'),
+  logInForm = document.querySelector('#logInForm'),
+  loginInput = document.querySelector('#login'),
+  userName = document.querySelector('.user-name'),
+  buttonOut = document.querySelector('.button-out');
+
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+  loginInput.style.borderColor = ''
+};
+
+buttonAuth.addEventListener('click', toggleModalAuth);
+closeAuth.addEventListener('click', toggleModalAuth);
+
+
+function authorized() {
+  function logOut() {
+    login = null;
+    localStorage.removeItem('gloDelivery');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  console.log('Authorized');
+  userName.textContent = login;
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  buttonOut.addEventListener('click', logOut);
+
+}
+
+function notAuthorized() {
+  console.log('Not authorized');
+
+  function logIn(e) {
+    e.preventDefault();
+    login = loginInput.value;
+    localStorage.setItem('gloDelivery', login);
+
+    if (login) {
+      toggleModalAuth();
+      buttonAuth.removeEventListener('click', toggleModalAuth);
+      closeAuth.removeEventListener('click', toggleModalAuth);
+      logInForm.removeEventListener('submit', logIn);
+
+      logInForm.reset();
+
+      checkAuth();
+    } else {
+      loginInput.style.borderColor = 'red';
+    }
+  }
+
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', logIn)
+
+
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
